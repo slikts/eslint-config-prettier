a8714cbd00218be4c6c3f98e59da17ac17d4b08e
slikts <dabas@untu.ms>
2018-09-06 11:54:00 +0300

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const { resolve } = require
+
+module.exports = {
+  extends: ['eslint-config-airbnb-base', 'eslint-config-prettier'].map(resolve),
+  plugins: ['prettier'],
+  rules: {
+    'func-names': [1, 'as-needed'],
+    quotes: [1, 'backtick'],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        semi: false,
+        // singleQuote: true,
+        printWidth: 100,
+      },
+    ],
+  },
+  env: {
+    es6: true,
+  },
+}
